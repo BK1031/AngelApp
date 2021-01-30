49c9ca364f6903d0577808daa87ffb8fe4485324
unknown <natasha.parker@warriorlife.net>
2021-01-30 12:06:42 -0800

    updated messages page

--- lib/pages/messages/messages_page.dart
+++ lib/pages/messages/messages_page.dart
@@ -21,28 +21,46 @@ class _MessagesPageState extends State<MessagesPage> {
             }),
         backgroundColor: Colors.black,
         body: Container(
-            margin: EdgeInsets.all(10),
+            margin: EdgeInsets.symmetric(vertical: 50, horizontal: 10),
             padding: (EdgeInsets.all(10)),
             color: Colors.blueGrey[800],
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                FlatButton(
-                    onPressed: () {},
-                    color: Colors.black,
-                    textColor: Colors.amber,
-                    child: Container(
-                        child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                          Container(
-                              child: Icon(Icons.person, color: Colors.black),
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle, color: Colors.amber)),
-                          Text('Jane Doe')
-                        ])))
-              ],
-            )));
+            child: ListView.separated(
+                separatorBuilder: (BuildContext context, int index) =>
+                    Divider(height: 10, thickness: 0),
+                itemCount: 15,
+                itemBuilder: (context, index) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      FlatButton(
+                          onPressed: () {},
+                          padding: EdgeInsets.all(20),
+                          color: Colors.black,
+                          textColor: Colors.amber,
+                          child: Container(
+                              child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                Container(
+                                    margin: EdgeInsets.only(right: 10),
+                                    child: Icon(Icons.person,
+                                        color: Colors.black, size: 30),
+                                    decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Colors.amber)),
+                                Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text('Jane Doe'),
+                                      Text(
+                                          'text post text post text post text post',
+                                          style: TextStyle(fontSize: 10))
+                                    ])
+                              ])))
+                    ],
+                  );
+                })));
   }
 }
