feb13ba429592baa5a20e63eb91bf4684b5a3650
unknown <natasha.parker@warriorlife.net>
2021-01-30 21:31:54 -0800

    made registration page UI, kinda

--- lib/pages/profile/sign_up_page.dart
+++ lib/pages/profile/sign_up_page.dart
@@ -13,12 +13,73 @@ class _SignUpPageState extends State<SignUpPage> {
     return Scaffold(
         body: Center(
             child: Container(
+                padding: EdgeInsets.all(20),
                 color: currBackgroundColor,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    TextField(decoration: InputDecoration(hintText: 'Email'))
+                    Image(
+                        height: 45, image: AssetImage('images/angel-logo.png')),
+                    TextField(
+                        cursorColor: accentColor,
+                        decoration: InputDecoration(
+                            labelText: 'Email',
+                            labelStyle: TextStyle(color: accentColor),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(width: 2)))),
+                    TextField(
+                        cursorColor: accentColor,
+                        decoration: InputDecoration(
+                            labelText: 'Username',
+                            labelStyle: TextStyle(color: accentColor),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(width: 2)))),
+                    TextField(
+                        cursorColor: accentColor,
+                        obscureText: true,
+                        obscuringCharacter: '*',
+                        decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(color: accentColor),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(width: 2)))),
+                    TextField(
+                        cursorColor: accentColor,
+                        obscureText: true,
+                        obscuringCharacter: '*',
+                        decoration: InputDecoration(
+                            labelText: 'Confirm Password',
+                            labelStyle: TextStyle(color: accentColor),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(width: 2)))),
+                    TextField(
+                        cursorColor: accentColor,
+                        decoration: InputDecoration(
+                            labelText: 'Date of Birth',
+                            labelStyle: TextStyle(color: accentColor),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(width: 2)))),
+                    CheckboxListTile(
+                        onChanged: (bool newValue) {
+                          setState(() {});
+                        },
+                        value: false,
+                        title: Text(
+                            'I accept the site Terms of Service and agree to the Privacy Policy.',
+                            style: TextStyle(fontSize: 12))),
+                    SizedBox(
+                        width: (MediaQuery.of(context).size.width / 2) - 40,
+                        height: 40,
+                        child: RaisedButton(
+                            color: accentColor,
+                            textColor: currBackgroundColor,
+                            child: Text('Submit'),
+                            onPressed: () {
+                              //go to main app
+                            })),
+                    RaisedButton(
+                        child: Text('Sign in with Google'), onPressed: () {})
                   ],
                 ))));
   }
